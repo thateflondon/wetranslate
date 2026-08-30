fbb57c17e616cc6034c3f30bb578f6c17d733751
thateflondon <jfpl.dev@gmail.com>
2026-08-30 23:35:44 +0200

    refactor: integrate TranslateForm into TranslateApp component

--- app/components/TranslateApp.tsx
+++ app/components/TranslateApp.tsx
@@ -1,15 +1,13 @@
 import Logo from "./Logo";
 import Background from "./Background"
+import TranslateForm from "./TranslateForm";
 
 export default function TranslateApp() {
   return (
     <div className="relative min-h-screen">
       <Logo/>
       <Background/>
-      <div className="container">
-        <div className="box detection">Detect language</div>
-        <div className="box translation">Translate langage</div>
-      </div>
+      <TranslateForm/>
     </div>
   );
 }
