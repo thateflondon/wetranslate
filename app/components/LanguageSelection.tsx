82375734ea94c876ba677bd73ff11ded97e1f12e
thateflondon <jfpl.dev@gmail.com>
2026-08-31 20:55:34 +0200

    feat(ui): add active state management to language selection

--- app/components/LanguageSelection.tsx
+++ app/components/LanguageSelection.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useState } from "react";
 import ExpandDownButton from "./ExpandDownButton"
 
 interface LanguageSelectionProps {
@@ -5,12 +7,19 @@ interface LanguageSelectionProps {
 }
 
 export default function LanguageSelection({ showDetectLanguage = true }: LanguageSelectionProps) {
+
+    //Add a class to the selected language
+    const [isActive, setIsActive] = useState(false);
+    const ToggleButton = () => {
+        setIsActive(!isActive);
+    }
+
   return (
     <div className="language-selector">
-      {showDetectLanguage && <button>Detect Language</button>}
-      <button>English</button>
-      <button>French</button>
-      <button className="flex">
+      {showDetectLanguage && <button onClick={ToggleButton} className={isActive ? "active" : ""} >Detect Language</button>}
+      <button className={isActive ? "active" : ""} onClick={ToggleButton}>English</button>
+      <button className={isActive ? "active" : ""} onClick={ToggleButton}>French</button>
+      <button className={`flex ${isActive ? "active" : ""}`} onClick={ToggleButton}>
         Spanish{" "}
         <ExpandDownButton/>{" "}
       </button>
--- app/components/TranslateForm.tsx
+++ app/components/TranslateForm.tsx
@@ -3,7 +3,7 @@ import LanguageSelection from "./LanguageSelection";
 
 export default function TranslateForm() {
   return (
-    <div className="container">
+    <div className="translate-form">
       <div className="box detection">
         <LanguageSelection showDetectLanguage={true} />
       </div>
