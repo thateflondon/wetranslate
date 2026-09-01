db2dd84480b45884a81f206f4e8c9a19914b44d9
thateflondon <jfpl.dev@gmail.com>
2026-09-01 02:50:20 +0200

    feat(ui): update TranslateForm with default languages and translated text display

--- app/components/TranslateForm.tsx
+++ app/components/TranslateForm.tsx
@@ -1,14 +1,52 @@
 import LanguageSelection from "./LanguageSelection";
-
+import TextToSpeech from "./TextToSpeech";
+import CopyText from "./CopyText";
+import TranslationButton from "./TranslationButton";
 
 export default function TranslateForm() {
   return (
     <div className="translate-form">
       <div className="box detection">
-        <LanguageSelection showDetectLanguage={true} />
+        {/* Set english as default text language to be translated && hide language detection button */}
+        <LanguageSelection showDetectLanguage={true} defaultLanguage="english" />
+        <textarea
+          name="input-content"
+          id="inputContent"
+          placeholder="Hello, how are you?"
+          maxLength={500}
+        />
+        <div className="counter-container">19/500</div>
+        <div className="tranlation-action-button">
+          <div className="sound-and-copy-button-container">
+            <TextToSpeech />
+            <CopyText />
+          </div>
+          <div className="translation-button-container">
+            <TranslationButton />
+          </div>
+        </div>
       </div>
       <div className="box translation">
-        <LanguageSelection showDetectLanguage={false} />
+        {/* Set french as default translated text language && hide language detection button */}
+        <LanguageSelection showDetectLanguage={false} defaultLanguage="french" />
+        <div className="translated-text-container">
+          <span id="translated-text">Bonjour, comment allez-vous ?</span>
+        </div>
+        {/* <textarea
+          name="output-content"
+          id="outputContent"
+          placeholder="Bonjour, comment allez-vous ?"
+        /> */}
+        <div className="tranlation-action-button">
+          <div className="sound-and-copy-button-container">
+            <TextToSpeech />
+            <CopyText />
+          </div>
+          <div className="translation-button-container">
+            {/* Hide the translation button on this side of form */}
+            {/* <TranslationButton /> */}
+          </div>
+        </div>
       </div>
     </div>
   );
