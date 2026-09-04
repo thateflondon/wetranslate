2b5c56ebc845d08e7468e8f3a0f95c46fd12f8d8
thateflondon <jfpl.dev@gmail.com>
2026-09-04 20:35:59 +0200

    style(responsive): refine breakpoints and improve spacing calculations

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -7,7 +7,7 @@ export default function Logo() {
       alt="WeTranslate logo"
       width={148}
       height={45}
-      className="w-full max-w-[148px] h-auto max-h-[45px] mt-[92px] mb-[52px] mx-auto"
+      className="w-full max-w-[148px] h-auto max-h-[45px] mb-[52px] mx-auto"
     />
   );
 }
